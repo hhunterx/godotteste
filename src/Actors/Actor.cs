150f6746fd0f2b786f094172a9dcf7a773623a7f
Willian <willian@spi.com.br>
2020-08-28 14:42:52 -0300

    completado video 1

---
+++ src/Actors/Actor.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public class Actor : KinematicBody2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	public Vector2 velocity = Vector2.Zero;
+
+	public Vector2 FLOOR_NORMAL = Vector2.Up;
+	
+	[Export]
+	public Vector2 speed {get;set;} = new Vector2(500, 1000);
+	
+	[Export]
+	public float gravity { get; set; } = 3000;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+	}
+
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _PhysicsProcess(float delta)
+	{
+		// velocity.y += gravity * delta;
+		// velocity.y = Math.Max(velocity.y, speed.y);
+		// velocity = this.MoveAndSlide(velocity);
+	}
+}
---
+++ src/Actors/Player.cs
@@ -0,0 +1,69 @@
+using Godot;
+using System;
+
+public class Player : Actor
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	[Export]
+	public float stomp_impulse = 1000;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+
+	}
+
+	public void _on_EnemyDetector_area_entered(Area2D body)
+	{
+		velocity = CalculateStompVelocity(velocity, stomp_impulse);
+	}
+
+	public void _on_EnemyDetector_body_entered(PhysicsBody2D body)
+	{
+		QueueFree();
+	}
+
+	public override void _PhysicsProcess(float delta)
+	{
+		//base._PhysicsProcess(delta);
+		var direction = GetDirection();
+		var isJumpInterrupted = Input.IsActionJustReleased("jump") && velocity.y < 0;
+
+		velocity = CalculateMoveVelocity(velocity, direction, speed, isJumpInterrupted);
+		velocity = MoveAndSlide(velocity, FLOOR_NORMAL);
+	}
+
+	public Vector2 GetDirection()
+	{
+		return new Vector2(
+			Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left"),
+			Input.IsActionJustPressed("jump") && IsOnFloor() ? -1 : 1);
+	}
+
+	public Vector2 CalculateMoveVelocity(Vector2 linerVelocity, Vector2 direction, Vector2 speed, bool isJumpInterrupted)
+	{
+		var result = linerVelocity;
+		result.x = speed.x * direction.x;
+		result.y += gravity * this.GetPhysicsProcessDeltaTime();
+		if (direction.y == -1)
+		{
+			result.y = speed.y * direction.y;
+		}
+
+		if (isJumpInterrupted)
+		{
+			result.y = 0;
+		}
+		return result;
+	}
+
+	public Vector2 CalculateStompVelocity(Vector2 linerVelocity, float impulse)
+	{
+		var reuslt = linerVelocity;
+		reuslt.y = -impulse;
+		return reuslt;
+	}
+}
